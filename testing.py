0ddab695f765b7b643d6fdf0619695f568a7382c
doctashay <shabennett2@gmail.com>
2020-09-24 14:29:00 -0600

    Some quick comments

--- testing.py
+++ testing.py
@@ -3,7 +3,7 @@ from common_utils import Utils
 
 class Tests(object):
     def run_tests():
-        Utils.create_and_modify()
-        Utils.test_jak()
-        Utils.list_contents()
+        Utils.create_and_modify() #Create a brand new ISO and output it to C:\
+        Utils.test_jak() #Open a hardcoded Jak ISO and list the contents (TODO: Modify 0COMMON.TXT)
+        Utils.list_contents() #Opens any specified ISO and lists the contents 
 
